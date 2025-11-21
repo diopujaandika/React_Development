0c59ad36275c953747e886ee4239381864f9dbc5
diopujaandika <dpujaandika17@gmail.com>
2025-11-21 09:22:20 +0700

    make a mybutton1

---
+++ Beginner/react2/technical-react/src/component/catalog_component/button/MyButton1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyButton1 extends React.Component{
+    render(){
+        return(
+            <div>
+                <button className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white" type="submit">
+                    Button
+                </button>
+            </div>
+        );
+    }
+}
+
+export default MyButton1;
--- Beginner/react2/technical-react/src/main.jsx
+++ Beginner/react2/technical-react/src/main.jsx
@@ -6,14 +6,18 @@ import MyComponent from './component/class_component/MyComponent.jsx'
 import MyComponent2 from './component/class_component/MyComponent2.jsx'
 import MyComponent3 from './component/class_component/MyComponent3.jsx'
 import Counter from './component/state_component/StateComponent.jsx'
+import MyButton1 from './component/catalog_component/button/MyButton1.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <div>
+    <div className=''>
       <MyComponent/>
       <MyComponent2 name="Dicoding Indonesia"/>
       <MyComponent3 name="Yayasan Dicoding Indonesia"/>
       <MyComponent3 name="Kota Bandung - Indonesia"/>
       <MyComponent3 name="Hello Coders!"/>
       <Counter/>
+      <MyButton1/>
+      <MyButton1/>
+      <MyButton1/>
     </div>
 )
