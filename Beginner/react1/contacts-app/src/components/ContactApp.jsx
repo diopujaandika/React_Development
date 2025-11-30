d9371702cc633d91e1d8f1a799d0bf06c700f59f
diopujaandika <dpujaandika17@gmail.com>
2025-11-30 14:45:30 +0700

    update

--- Beginner/react1/contacts-app/src/components/ContactApp.jsx
+++ Beginner/react1/contacts-app/src/components/ContactApp.jsx
@@ -11,7 +11,7 @@ class ContactApp extends React.Component {
    }
  
    this.onDeleteHandler = this.onDeleteHandler.bind(this);
-   this.onAddContactHandler = this.onAddContactHandler(this);
+   this.onAddContactHandler = this.onAddContactHandler.bind(this);
  }
  
  onDeleteHandler(id) {
