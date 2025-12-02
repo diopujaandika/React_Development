03ca8eed39a8dbe85424ee1ac0b6ac13247a63e7
diopujaandika <dpujaandika17@gmail.com>
2025-12-02 21:13:17 +0700

    update

--- Beginner/react2/technical-react/src/pages/login.jsx
+++ Beginner/react2/technical-react/src/pages/login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import FormLogin from "../component/fragments/FormLogin";
 import AuthLayout from "../component/layouts/AuthLayouts";
 
@@ -5,6 +6,9 @@ const LoginPage = () => {
     return (
         <AuthLayout title = "Login">
             <FormLogin/>
+            <p className="text-sm mt-5 text-center">Don't have an account?
+                <Link className="font-bold text-blue-600 " to="/register"> Register</Link> 
+            </p>
         </AuthLayout>
     )
 }
--- Beginner/react2/technical-react/src/pages/register.jsx
+++ Beginner/react2/technical-react/src/pages/register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import FormRegister from "../component/fragments/FormRegister";
 import AuthLayout from "../component/layouts/AuthLayouts";
 
@@ -5,6 +6,9 @@ const RegisterPage = () => {
     return (
         <AuthLayout title = "Register">
             <FormRegister/>
+            <p className="text-sm mt-5 text-center">Have an account?
+                <Link className="font-bold text-blue-600 " to="/login"> Login</Link> 
+            </p>
         </AuthLayout>
     )
 }
